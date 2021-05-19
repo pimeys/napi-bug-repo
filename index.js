edbb1a39e5fe04b605ee0d6ebc75875331c57ca0
Julius de Bruijn <julius+github@nauk.io>
2021-05-19 19:57:24 +0200

    Memory leak test

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const api = require(`./libnapi_test.so.node`)
 
 async function main() {
-    const a = new api.A((s) => console.log(s));
-    const b = new api.A((s) => console.log(s));
+    while (true) {
+        const q = `query {
+            findFirstBooking(where: { id: "ckovh15xa104945sj64rdk8oas" }) {
+                id
+                name
+                forename
+                description
+                email
+                phone
+                arrivalDate
+                departureDate
+                price
+                advance
+                advanceDueDate
+                kids
+                adults
+                status
+                nourishment
+                createdAt
+                room {
+                id
+                name
+                }
+            }
+        }`;
+
+        let query = JSON.stringify({
+            query: q,
+            variables: {}
+        });
+
+        let res = await api.test(query);
+        console.log(JSON.parse(res));
+    }
 }
 main()
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,40 @@
-use napi::{
-    threadsafe_function::{ThreadSafeCallContext, ThreadsafeFunction},
-    CallContext, Env, JsFunction, JsObject, JsUndefined,
-};
+use napi::{CallContext, Env, JsObject, JsString};
 use napi_derive::{js_function, module_exports};
 
-#[derive(Clone)]
-struct A {
-    cb: ThreadsafeFunction<String>,
-}
-
 #[js_function(1)]
-fn constructor(ctx: CallContext) -> napi::Result<JsUndefined> {
-    let callback = ctx.get::<JsFunction>(0)?;
+fn test(ctx: CallContext) -> napi::Result<JsObject> {
+    let _ = ctx.get::<JsString>(0)?.into_utf8()?.into_owned()?;
 
-    let mut cb = ctx
-        .env
-        .create_threadsafe_function(&callback, 0, |ctx: ThreadSafeCallContext<String>| {
-            ctx.env
-                .create_string_from_std(ctx.value)
-                .map(|js_string| vec![js_string])
-        })?;
+    let data = serde_json::json!({
+        "findFirstBooking": {
+            "id": "ckovh15xa104945sj64rdk8oas",
+            "name": "1883da9ff9152",
+            "forename": "221c99bedc6a4",
+            "description": "8bf86b62ce6a",
+            "email": "9d57a869661cc",
+            "phone": "7e0c58d147215",
+            "arrivalDate": -92229669,
+            "departureDate": 202138795,
+            "price": -1592700387,
+            "advance": -369294193,
+            "advanceDueDate": 925000428,
+            "kids": 520124290,
+            "adults": 1160258464,
+            "status": "NO_PAYMENT",
+            "nourishment": "BB",
+            "createdAt": "2021-05-19T12:58:37.246Z",
+            "room": { "id": "ckovh15xa104955sj6r2tqaw1c", "name": "38683b87f2664" }
+        }
+    });
 
-    cb.unref(&ctx.env)?;
-
-    let mut this: JsObject = ctx.this_unchecked();
-    let obj = A { cb };
-
-    ctx.env.wrap(&mut this, obj)?;
-    ctx.env.get_undefined()
+    ctx.env.execute_tokio_future(
+        async move { Ok(serde_json::to_string(&data).unwrap()) },
+        |&mut env, response| env.create_string(&response),
+    )
 }
 
 #[module_exports]
-pub fn init(mut exports: JsObject, env: Env) -> napi::Result<()> {
-    let obj = env.define_class("A", constructor, &[])?;
-
-    exports.set_named_property("A", obj)?;
-
+pub fn init(mut exports: JsObject, _env: Env) -> napi::Result<()> {
+    exports.create_named_method("test", test)?;
     Ok(())
 }
