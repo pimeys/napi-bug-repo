3a3b4704fe06a55579b66d29d574181e09c049e4
Julius de Bruijn <julius+github@nauk.io>
2021-06-11 17:52:34 +0200

    Test for callback abort

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
 const api = require(`./libnapi_test.so.node`)
 
 async function main() {
-    while (true) {
-        const q = `query {
-            findFirstBooking(where: { id: "ckovh15xa104945sj64rdk8oas" }) {
-                id
-                name
-                forename
-                description
-                email
-                phone
-                arrivalDate
-                departureDate
-                price
-                advance
-                advanceDueDate
-                kids
-                adults
-                status
-                nourishment
-                createdAt
-                room {
-                id
-                name
-                }
-            }
-        }`;
+    let engine = new api.Engine((_err, log) => {
+        console.log(log);
+    });
 
-        let query = JSON.stringify({
-            query: q,
-            variables: {}
-        });
-
-        let res = await api.test(query);
-        console.log(JSON.parse(res));
+    for (let i = 0; i < 10000; i++) {
+        await engine.call("call_no_" + i);
     }
+
+    await engine.off();
 }
 main()
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,101 @@
-use napi::{CallContext, Env, JsObject, JsString};
+use std::sync::{Arc, RwLock};
+
+use napi::{
+    threadsafe_function::{ThreadSafeCallContext, ThreadsafeFunction},
+    CallContext, Env, JsFunction, JsObject, JsString, JsUndefined, Property,
+};
 use napi_derive::{js_function, module_exports};
 
+enum Mode {
+    On(ThreadsafeFunction<String>),
+    Off,
+}
+
+#[derive(Clone)]
+struct Engine {
+    mode: Arc<RwLock<Mode>>,
+}
+
 #[js_function(1)]
-fn test(ctx: CallContext) -> napi::Result<JsObject> {
-    let _ = ctx.get::<JsString>(0)?.into_utf8()?.into_owned()?;
+pub fn constructor(ctx: CallContext) -> napi::Result<JsUndefined> {
+    let cb = ctx.get::<JsFunction>(0)?;
 
-    let data = serde_json::json!({
-        "findFirstBooking": {
-            "id": "ckovh15xa104945sj64rdk8oas",
-            "name": "1883da9ff9152",
-            "forename": "221c99bedc6a4",
-            "description": "8bf86b62ce6a",
-            "email": "9d57a869661cc",
-            "phone": "7e0c58d147215",
-            "arrivalDate": -92229669,
-            "departureDate": 202138795,
-            "price": -1592700387,
-            "advance": -369294193,
-            "advanceDueDate": 925000428,
-            "kids": 520124290,
-            "adults": 1160258464,
-            "status": "NO_PAYMENT",
-            "nourishment": "BB",
-            "createdAt": "2021-05-19T12:58:37.246Z",
-            "room": { "id": "ckovh15xa104955sj6r2tqaw1c", "name": "38683b87f2664" }
+    let mut cb = ctx
+        .env
+        .create_threadsafe_function(&cb, 0, |mut ctx: ThreadSafeCallContext<String>| {
+            ctx.env.adjust_external_memory(ctx.value.len() as i64)?;
+
+            ctx.env
+                .create_string_from_std(ctx.value)
+                .map(|js_string| vec![js_string])
+        })?;
+
+    cb.unref(&ctx.env)?;
+
+    let mut this: JsObject = ctx.this_unchecked();
+    let mode = Mode::On(cb);
+    let engine = Engine {
+        mode: Arc::new(RwLock::new(mode)),
+    };
+
+    ctx.env.wrap(&mut this, engine)?;
+    ctx.env.get_undefined()
+}
+
+#[js_function(1)]
+fn call(ctx: CallContext) -> napi::Result<JsObject> {
+    let prefix = ctx.get::<JsString>(0)?.into_utf8()?.as_str()?.to_string();
+
+    let this: JsObject = ctx.this_unchecked();
+    let engine: &Engine = ctx.env.unwrap(&this)?;
+    let engine: Engine = engine.clone();
+
+    let future = async move {
+        for i in 1..10 {
+            match &*engine.mode.read().unwrap() {
+                Mode::On(cb) => {
+                    cb.call(
+                        Ok(format!("{}_{}", prefix, i)),
+                        napi::threadsafe_function::ThreadsafeFunctionCallMode::NonBlocking,
+                    );
+                }
+                Mode::Off => (),
+            }
         }
-    });
 
-    ctx.env.execute_tokio_future(
-        async move { Ok(serde_json::to_string(&data).unwrap()) },
-        |&mut env, response| env.create_string(&response),
-    )
+        Ok(())
+    };
+
+    ctx.env.execute_tokio_future(future, |env, ()| env.get_undefined())
+}
+
+#[js_function(0)]
+fn off(ctx: CallContext) -> napi::Result<JsObject> {
+    let this: JsObject = ctx.this_unchecked();
+    let engine: &Engine = ctx.env.unwrap(&this)?;
+    let engine: Engine = engine.clone();
+
+    let future = async move {
+        let mut mode = engine.mode.write().unwrap();
+        *mode = Mode::Off;
+        Ok(())
+    };
+
+    ctx.env.execute_tokio_future(future, |env, ()| env.get_undefined())
 }
 
 #[module_exports]
-pub fn init(mut exports: JsObject, _env: Env) -> napi::Result<()> {
-    exports.create_named_method("test", test)?;
+pub fn init(mut exports: JsObject, env: Env) -> napi::Result<()> {
+    let engine = env.define_class(
+        "Engine",
+        constructor,
+        &[
+            Property::new(&env, "call")?.with_method(call),
+            Property::new(&env, "off")?.with_method(off),
+        ],
+    )?;
+
+    exports.set_named_property("Engine", engine)?;
+
     Ok(())
 }
